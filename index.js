d88556d85abc8a75a94fc88c4a6743ac1b773017
Jason Frame <jason@onehackoranother.com>
2014-01-01 17:10:39 +0000

    add gl-matrix and deg2rad utility

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var glm = require('gl-matrix');
+
 module.exports = {
-    Scene   : require('./lib/Scene')
+
+    //
+    // export gl-matrix
+
+    mat2        : glm.mat2,
+    mat3        : glm.mat3,
+    mat4        : glm.mat4,
+    quat        : glm.quat,
+    vec2        : glm.vec2,
+    vec3        : glm.vec3,
+    vec4        : glm.vec4,
+    
+    //
+    // utility functions
+
+    deg2rad     : require('./lib/util/deg2rad'),
+
+    //
+    // drake
+    
+    Scene       : require('./lib/Scene')
+
 };
